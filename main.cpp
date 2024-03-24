d33c24fb754cb12143cb2a3f15edaaf074ff443d
Darko Tomic <darkotomic@Darkos-MacBook-Pro.local>
2024-03-24 22:49:39 +0100

    Added commands array

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 
 void AddCalories()
 {
- 
     std::string itemName = "";
     int calories = 0;
      
@@ -12,25 +11,32 @@ void AddCalories()
     std::cout << "Enter calories: " << std::endl;
     std::cin >> calories;  
     std::cout << "You added item " << itemName << " with " << calories << " calories to a database." << std::endl;
-    
 }
 
 void StartApplication()
 {
-    std::string commandHelp = "";
-    std::cout << "Type command </add> to start the applicaiton. Enter /help for help.To quit enter q" << std::endl;
-    std::cin >> commandHelp;
+    std::string commandsArray[10];
+    commandsArray[0] = "/help"; 
+    commandsArray[1] = "/add";
+    commandsArray[2] = "/remove";  
 
-    if(commandHelp == "/add")
+    std::string command = "";
+    std::cout << "Type command </add> to start the applicaiton. Enter /help for help.To quit enter q" << std::endl;
+    std::cin >> command;
+
+    for(int i = 0; i < commandsArray.size(); i++)
     {
-        std::cout << "This is a program for adding calories to data base" << std::endl;
-        AddCalories();
-    }
-    else
-    {
-        std::string commandHelp = "";
-        std::cout << "Type command </add> to start the applicaiton. Enter /help for help." << std::endl;
-        std::cin >> commandHelp;
+        if(command == comandsArray[i])
+        {
+            std::cout << "This is a program for adding calories to data base" << std::endl;
+            AddCalories();
+        }
+        else
+        {
+            std::string commandHelp = "";
+            std::cout << "Type command </add> to start the applicaiton. Enter /help for help." << std::endl;
+            std::cin >> commandHelp;
+        }
     }
 }
 
