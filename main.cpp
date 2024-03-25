52b08bb2dd674b7b58dab85e2e1175146f72cf23
Darko Tomic <darkotomic@Darkos-MacBook-Pro.local>
2024-03-25 13:53:40 +0100

    Refactored commands array and changed command names

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <string>
 
-void AddCalories()
+void addCalories()
 {
     std::string itemName = "";
-    int calories = 0;
+    int calories = {0};
      
     std::cout << "Enter item name" << std::endl;
     std::cin >> itemName;
@@ -13,29 +14,24 @@ void AddCalories()
     std::cout << "You added item " << itemName << " with " << calories << " calories to a database." << std::endl;
 }
 
-void StartApplication()
+void startApplication()
 {
     std::string commandsArray[10];
-    commandsArray[0] = "/help"; 
-    commandsArray[1] = "/add";
-    commandsArray[2] = "/remove";  
+    commandsArray[0] = "a"; 
+    commandsArray[1] = "h";
+    commandsArray[2] = "q";  
 
     std::string command = "";
-    std::cout << "Type command </add> to start the applicaiton. Enter /help for help.To quit enter q" << std::endl;
+    std::cout << "Type command (a)add to start the applicaiton. Enter (h)elp for help.To quit enter (q)uit." << std::endl;
     std::cin >> command;
 
-    for(int i = 0; i < commandsArray.size(); i++)
+    for(int i = 0; i < sizeof(commandsArray); i++)
     {
-        if(command == comandsArray[i])
+        if(command == commandsArray[i])
         {
             std::cout << "This is a program for adding calories to data base" << std::endl;
-            AddCalories();
-        }
-        else
-        {
-            std::string commandHelp = "";
-            std::cout << "Type command </add> to start the applicaiton. Enter /help for help." << std::endl;
-            std::cin >> commandHelp;
+            addCalories();
+            break;
         }
     }
 }
@@ -47,7 +43,7 @@ int main()
     std::cout << "Visit our website at tomiczengineering.com for more information." << std::endl;
     std::cout << "\n";
 
-    StartApplication();
+    startApplication();
    
     return 0;
 }
