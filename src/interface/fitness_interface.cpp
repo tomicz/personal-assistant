56bd317ee4da4480d3156316d6b6512382b2aadb
Tomicz Engineering LLC <tomiczdarko@gmail.com>
2024-10-17 04:20:58 +0200

    Added delete and list plans

--- src/interface/fitness_interface.cpp
+++ src/interface/fitness_interface.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <filesystem>
 #include <fstream>
+#include <algorithm>
 #include "../../include/fitness_interface.hpp"
 #include "../../include/user_interface.hpp"
 #include "../../include/exercise_controller.hpp"
@@ -151,7 +152,58 @@ namespace fitness{
     }
 
     void FitnessInterface::delete_plan(){
-        std::cout << "Delete plan\n";
+        const std::string RED_COLOR = "\033[1;31m";
+        const std::string RESET_COLOR = "\033[0m";
+
+        std::filesystem::path plans_path = "../db/fitness";
+        if(std::filesystem::exists(plans_path)){
+            std::string plan_name;
+
+            int index = 1;
+            for(const auto& file: std::filesystem::directory_iterator(plans_path)){
+                if(file.path().filename().string().find(".txt") != std::string::npos){    
+                    std::string file_name = file.path().filename().string();
+                    file_name.erase(file_name.size() - 4, 4);
+                    std::cout << index << ". " << file_name << std::endl;
+                    index++;
+                }
+            }
+
+            std::cout << std::endl;
+            std::cout << "Select plan to delete: ";
+            int plan_index;
+            std::cin >> plan_index;
+
+            int current_index = 1;
+            for(const auto& file: std::filesystem::directory_iterator(plans_path)){
+                std::string file_name = file.path().filename().string();
+                file_name.erase(file_name.size() - 4, 4);
+                if(current_index == plan_index){
+                    std::cout << "Are you sure you want to delete plan " << file_name << "? (y/n): ";
+                    char command;
+                    std::cin >> command;
+                    std::cout << std::endl;
+                    if(command == 'y'){
+                        std::filesystem::remove(file.path());
+                        std::cout << RED_COLOR << "Plan " << file_name << " deleted successfully." << RESET_COLOR << std::endl;
+                        list_plans();
+                    }
+                    else if(command == 'n'){
+                        open_plan_maker();
+                    }
+                    else{
+                        std::cout << "Invalid command\n";
+                    }
+                    break;
+                }
+                current_index++;
+            }
+        }
+        else{
+            std::cout << "No plans found.\n";
+        }
+
+        open_plan_maker();
     }
 
     void FitnessInterface::update_plan(){
@@ -159,7 +211,27 @@ namespace fitness{
     }
 
     void FitnessInterface::list_plans(){
-        std::cout << "List plans\n";
+        UI ui;
+        ui.set_header("All Plans");
+        std::filesystem::path fitness_dir = "../db/fitness";
+
+        if(std::filesystem::exists(fitness_dir)){
+            int index = 1;
+            for(const auto& file: std::filesystem::directory_iterator(fitness_dir)){
+                std::string file_name = file.path().filename().string();
+                if(file_name.find(".txt") != std::string::npos){    
+                    file_name.erase(file_name.size() - 4, 4);
+                    std::cout << index << ". " << file_name << std::endl;
+                    index++;
+                }
+            }
+            std::cout << std::endl;
+        }
+        else{
+            std::cout << "No plans found.\n";
+        }
+
+        open_plan_maker();
     }
 
     std::string FitnessInterface::create_fitness_folder(){
